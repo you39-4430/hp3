4630ee5468c4156039c44463cb62f337aa84b5b7
吉澤勇作 <>
2022-08-02 22:15:53 +0900

    会社概要ページ 完了

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(function () {
         var hTop = $(this).offset().top;
         var hHeight = $(this).height();
       var hBottom = hTop + hHeight;
-      var wTop = $(window).scrollTop();
+      var wTop = $(window).scrollTop() + 150;
       var wHeight = $(window).height();
       var wBottom = wTop + wHeight;
       if (hBottom <= wBottom) {
@@ -39,7 +39,7 @@ $(function () {
       var hTop = $(this).offset().top;
       var hHeight = $(this).height();
       var hBottom = hTop + hHeight;
-      var wTop = $(window).scrollTop();
+      var wTop = $(window).scrollTop() + 150;
       var wHeight = $(window).height();
       var wBottom = wTop + wHeight;
       if (hBottom <= wBottom) {
@@ -52,6 +52,53 @@ $(function () {
     });
   });
   //要素が画面の範囲に入ったら下からフェードイン ↑
+
+  //要素が画面の範囲に入ったら右からフェードイン ↓
+  $(".js-rightShow").css({
+      opacity: "0",
+      left: "100px",
+    });
+    $(window).scroll(function () {
+        $(".js-rightShow").each(function () {
+      var hTop = $(this).offset().top;
+      var hHeight = $(this).height();
+      var hBottom = hTop + hHeight;
+      var wTop = $(window).scrollTop() + 150;
+      var wHeight = $(window).height();
+      var wBottom = wTop + wHeight;
+      if (hBottom <= wBottom) {
+          $(this).css({ 
+            transition: "all 0.5s ease",
+            left: "0",
+            opacity: "1" 
+        });
+      }
+    });
+  });
+  //要素が画面の範囲に入ったら右からフェードイン ↑
+  //要素が画面の範囲に入ったら左からフェードイン ↓
+  $(".js-leftShow").css({
+      opacity: "0",
+      right: "100px",
+    });
+    $(window).scroll(function () {
+        $(".js-leftShow").each(function () {
+      var hTop = $(this).offset().top;
+      var hHeight = $(this).height();
+      var hBottom = hTop + hHeight;
+      var wTop = $(window).scrollTop() + 150;
+      var wHeight = $(window).height();
+      var wBottom = wTop + wHeight;
+      if (hBottom <= wBottom) {
+          $(this).css({ 
+            transition: "all 0.5s ease",
+            right: "0",
+            opacity: "1" 
+        });
+      }
+    });
+  });
+  //要素が画面の範囲に入ったら左からフェードイン ↑
 
   //ホバー時したから要素を表示 ↓
   $(".js-slideDown__point").hover(
